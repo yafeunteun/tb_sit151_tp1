7c80ef09a4419e77a1bcbed04058a0db63dc6c40
y28 <yann.andreu@yahoo.com>
2014-09-11 13:44:06 +0200

    Correction cryptage

--- exo3/exo3.c
+++ exo3/exo3.c
@@ -27,11 +27,11 @@ void crypte(char texte[], int longueur, char chaineCryptage[], int clefCryptage)
 	int m = strlen(chaineCryptage);
 	srand(clefCryptage);	
 
-	longueur = longueur <= strlen(texte) ? longueur : strlen(texte);
+	longueur = longueur <= strlen(texte) ? longueur : strlen(texte); // Si l'utilisateur veut crypter plus de caractères que la chaine n'en contient, on crypte jusqu'au dernier caractère de la chaine.
 
 	for(i = 0; i < longueur; ++i) {
 		char c = (char)rand();
-		texte[i] = c^chaineCryptage[i%m];
+		texte[i] = c^chaineCryptage[i%m]^texte[i];
 	}
 	
 }
